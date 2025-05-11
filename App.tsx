de7ae6cf4f11e45e3305032e405459d54b423d44
raauulsiilvaa <raulsilvaalonso07@gmail.com>
2025-05-11 15:49:54 +0200

    Creando estructura y app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import MainNavigator from './navigation/MainNavigator';
+import { EventProvider } from './context/EventContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <EventProvider>
+      <NavigationContainer>
+        <MainNavigator />
+      </NavigationContainer>
+    </EventProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
